a5449e10212fdf8dcea4956c5c15aa5283b41790
hvg-workshop <1139375659@qq.com>
2026-03-20 13:49:10 +0800

    feat:移除

--- src/content/workshop-content.js
+++ src/content/workshop-content.js
@@ -1,5 +1,5 @@
 export const workshopContent = {
-  brandLabel: 'HVG 2026',
+  // brandLabel: 'HVG 2026',
   tabs: [
     { id: 'overview', label: 'OVERVIEW' },
     { id: 'topics', label: 'Topics' },
